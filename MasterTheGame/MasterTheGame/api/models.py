c83e704bfd83fe799b30ece96e2201e7dc49ce16
sreejit pv <sreejith.pv@sports4you.com>
2016-02-23 20:03:05 +0530

    model changes

--- MasterTheGame/MasterTheGame/api/models.py
+++ MasterTheGame/MasterTheGame/api/models.py
@@ -9,9 +9,14 @@ class Buddy(models.Model):
 
     EXPERTISE_CHOICES = (("CERTIFIED","Certified"),("TRAINEE","Trainee"))
     TYPE_CHOICES = (("LEAD","Lead"),("SUPPORT","Support"))
-
+    name = models.CharField(max_length=100,null=True,blank=True)
     expertise = models.CharField(max_length=10, choices=EXPERTISE_CHOICES, null=True,blank=True)
     type = models.CharField(max_length=10, choices=TYPE_CHOICES, null=True,blank=True)
+    phone = models.CharField(max_length=100,null=True,blank=True)
+    age = models.IntegerField(null=True,blank=True)
+    user_name = models.CharField(max_length=100,null=True,blank=True)
+    password = models.CharField(max_length=100)
+    sports4You_id = models.CharField(max_length=50)
 
 
 
@@ -24,7 +29,6 @@ class Package(models.Model):
     session_duration = models.IntegerField(null=True, blank=True)
     def __str__(self):
         return self.name
-
 
 
 class Player(models.Model):
@@ -48,63 +52,79 @@ class Player(models.Model):
 
 
 
-class Programs(models.Model):
-
+class Sports(models.Model):
     Name = models.CharField(max_length=100,null=True,blank=True)
     description = models.TextField(null=True,blank=True)
     def __str__(self):
         return self.Name
 
 
-class Sports(models.Model):
-    Name = models.CharField(max_length=100,null=True,blank=True)
-    def __str__(self):
-        return self.Name
-
-
 class Location(models.Model):
-    State = models.CharField(max_length=100,null=True,blank=True)
-    City = models.CharField(max_length=100,null=True,blank=True)
-    Area = models.CharField(max_length=100,null=True,blank=True)
-    CENTER_CHOICES = (("MAGNUM","MAGNUM"),("PLAY","PLAY"))
-    center = models.CharField(max_length=10, choices=CENTER_CHOICES, null=True,blank=True)
+    City = models.ForeignKey("City")
+    center = models.CharField(max_length=10, null=True,blank=True)
     latitude = models.DecimalField(max_digits=12, decimal_places=2)
     longitude = models.DecimalField(max_digits=12, decimal_places=2)
-    upload_path = '/img'
-    face_picture = models.ImageField(upload_to=upload_path)
+    face_picture = models.ImageField(upload_to='location')
     address = models.TextField(null=True,blank=True)
     centre_head = models.CharField(max_length=100,null=True,blank=True)
     centre_manager = models.CharField(max_length=100,null=True,blank=True)
     contact_number = models.CharField(max_length=12)
     sports = models.ForeignKey(Sports, null=True, blank=True)
-    location = models.ForeignKey('Location', null=True, blank=True)
-    program = models.ForeignKey(Programs, null=True, blank=True)
 
 
-class Sports_Location(models.Model):
+
+class City(models.Model):
+    name = models.CharField(max_length=100,null=True,blank=True)
+    state = models.ForeignKey("State")
+
+class State(models.Model):
+    name = models.CharField(max_length=100,null=True,blank=True)
+
+
+class Slot(models.Model):
     sports = models.ForeignKey(Sports, null=True, blank=True)
     location = models.ForeignKey(Location, null=True, blank=True)
-    program = models.ForeignKey(Programs, null=True, blank=True)
-
-class Batch(models.Model):
-    sports = models.ForeignKey(Sports, null=True, blank=True)
-    location = models.ForeignKey(Location, null=True, blank=True)
-    program = models.ForeignKey(Programs, null=True, blank=True)
+    start_time= models.DateTimeField(blank=True, null=True)
+    end_time= models.DateTimeField(blank=True, null=True)
 
 class Session(models.Model):
     session_date = models.DateField(null=True, blank=True)
     session_number = models.IntegerField(null=True, blank=True)
-    batch = models.ForeignKey(Batch, null=True, blank=True)
+    batch = models.ForeignKey('Slot', null=True, blank=True)
     buddy_summary = models.TextField(null=True,blank=True)
     physic_attended = models.BooleanField(default=True)
+    players= models.ManyToManyField('Player',null=True,blank=True)
+    buddy = models.ForeignKey('Buddy',null=True,blank=True)
+
 
 class Session_player(models.Model):
-    Player = models.ForeignKey(Player, null=True, blank=True)
-    Session = models.ForeignKey(Session, null=True, blank=True)
+    Player = models.ForeignKey('Player', null=True, blank=True)
+    Session = models.ForeignKey('Session', null=True, blank=True)
     buddy_rating = models.IntegerField(null=True, blank=True)
     session_rating = models.IntegerField(null=True, blank=True)
     is_confirmed =  models.BooleanField(default=True)
     feedback = models.TextField(null=True,blank=True)
+
 class Team(models.Model):
-    Session = models.ForeignKey(Sports, null=True, blank=True)
+    Session = models.ForeignKey('Session', null=True, blank=True)
     buddy = models.ForeignKey(Buddy, null=True, blank=True)
+    players = models.ManyToManyField('Player')
+
+
+class Buddy_rating(models.Model):
+    value = models.FloatField(default=2.5)
+    rated_by = models.ForeignKey('Player')
+    buddy = models.ForeignKey('Buddy')
+    slot =models.ForeignKey('Session')
+
+class Player_rating(models.Model):
+    value = models.FloatField(default=2.5)
+    rated_by = models.ForeignKey('Buddy')
+    buddy = models.ForeignKey('Player')
+    slot =models.ForeignKey('Session')
+
+
+class Message(models.Model):
+    body = models.TextField()
+    message_from = models.ForeignKey('Player')
+    message_to = models.ForeignKey('Buddy')
