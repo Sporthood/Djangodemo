eb88e0bc59cb6cacdf4c90194dd7db26e9254275
sreejit pv <sreejith.pv@sports4you.com>
2016-02-28 19:06:43 +0530

    login api

---
+++ MasterTheGame/MasterTheGame/api/decorator.py
@@ -0,0 +1,69 @@
+from django.http import HttpResponse
+import json as simplejson
+from django.contrib.sessions.backends.db import SessionStore
+from functools import wraps
+import logging
+
+
+
+log = logging.getLogger('api')
+
+# Create your views here.
+def checkinput(method):
+    def wrapper(func):
+        def inner_decorator(request, *args, **kwargs):
+            if request.method.upper() == method.upper():
+                req = json_request(request)
+                if req is not None:
+                    if func.__name__ not in ['login',
+                                             'signup'
+                                            ]:
+                        log.info('API : '+func.__name__+', Input: '+str(req))
+
+                    return func(request, req, *args, **kwargs)
+                else:
+                    log.error('API : Got a request with non-JSON input, Rejected.')
+                    return custom_error('Please enter a valid JSON input')
+            else:
+                log.error('API : Got a '+method.upper()+' request, Rejected.')
+                return custom_error('The Requested method is not allowed')
+        return wraps(func)(inner_decorator)
+    return wrapper
+
+
+
+
+def custom_error(message):
+    return json_response({'status': 0, 'error_message': message})
+
+
+def json_response(response, wrap=False):
+    if wrap:
+        final_response = {"data": response}
+    else:
+        final_response = response
+    #header_res = HttpResponse(simplejson.dumps(final_response))
+    header_res = (HttpResponse(simplejson.dumps(final_response), content_type="application/json"))
+
+    return header_res
+
+
+def json_request(request):
+    if request.method == 'GET':
+        req = request.GET
+        return req
+    else:
+        req = request.body
+        if not req:
+            req='{"a":"b"}'
+    if (req):
+        try:
+            if request.FILES:
+                return request.POST
+            else:
+                return simplejson.loads(req, "ISO-8859-1")
+        except Exception as e:
+            log.error("Error json-decoding input : " +e.message)
+            return None
+    else:
+        return None
--- MasterTheGame/MasterTheGame/api/views.py
+++ MasterTheGame/MasterTheGame/api/views.py
@@ -1,52 +1,27 @@
 from django.shortcuts import render
 
 from django.views.decorators.csrf import csrf_exempt
-from django.core.validators import validate_email
 from api.models import *
 from django.http import HttpResponse
 import json as simplejson
 from django.utils.crypto import get_random_string
-from django.http import HttpResponse
-import json as simplejson
 from django.contrib.sessions.backends.db import SessionStore
-from functools import wraps
-import logging
+import md5
+from decorator import *
 
 
-log = logging.getLogger('api')
 
-# Create your views here.
-def checkinput(method):
-    def wrapper(func):
-        def inner_decorator(request, *args, **kwargs):
-            if request.method.upper() == method.upper():
-                req = json_request(request)
-                if req is not None:
-                    if func.__name__ not in ['login',
-                                             'signup'
-                                            ]:
-                        log.info('API : '+func.__name__+', Input: '+str(req))
-
-                    return func(request, req, *args, **kwargs)
-                else:
-                    log.error('API : Got a request with non-JSON input, Rejected.')
-                    return custom_error('Please enter a valid JSON input')
-            else:
-                log.error('API : Got a '+method.upper()+' request, Rejected.')
-                return custom_error('The Requested method is not allowed')
-        return wraps(func)(inner_decorator)
-    return wrapper
 
 @csrf_exempt
 @checkinput('POST')
-def signup(data,request):
+def signup(request,data):
     try:
-        name = request['name'].strip()
-        phone = request['phone'].strip()
-        age = request['age']
-        user_name = str(request['user_name']).strip()
-        password = str(request['password']).strip()
-        type = request['role'].strip()
+        name = data['name'].strip()
+        phone = data['phone'].strip()
+        age = data['age']
+        user_name = str(data['user_name']).strip()
+        password = str(data['password']).strip()
+        type = data['role'].strip()
     except Exception as E:
         return custom_error("signup failed")
     if len(password) < 6:
@@ -64,6 +39,7 @@ def signup(data,request):
         buddy.user_name=user_name
         buddy.sports4You_id=s4u_id
         buddy.type= type
+        buddy.password= md5.new(password).hexdigest()
         buddy.save()
 
 
@@ -82,40 +58,29 @@ def signup(data,request):
 
 
 
-def custom_error(message):
-    return json_response({'status': 0, 'error_message': message})
+@csrf_exempt
+@checkinput('POST')
+def login(request, data):
+
+    try:
+        user_name = str(data['user_name']).strip()
+        password = str(data['password']).strip()
+        buddy = Buddy.objects.get(user_name=user_name,password = md5.new(password).hexdigest())
+    except Exception as e:
+        return custom_error("Please verify login details")
+
+    user_dictionary = {"name": buddy.name, "phone": buddy.phone, "age": buddy.age,"user_name": buddy.user_name,"sports4You_id": buddy.sports4You_id,"type": buddy.type}
+    session = SessionStore()
+    session["user"] = user_dictionary
+    session.set_expiry(3000)
+    session.set_test_cookie()
+    session.save()
+
+    response = {"status": 1, "user": session["user"], "session_key": session.session_key, "message": "login success"}
+    return json_response(response)
 
 
-def json_response(response, wrap=False):
-    if wrap:
-        final_response = {"data": response}
-    else:
-        final_response = response
-    #header_res = HttpResponse(simplejson.dumps(final_response))
-    header_res = (HttpResponse(simplejson.dumps(final_response), content_type="application/json"))
 
-    return header_res
-
-
-def json_request(request):
-    if request.method == 'GET':
-        req = request.GET
-        return req
-    else:
-        req = request.body
-        if not req:
-            req='{"a":"b"}'
-    if (req):
-        try:
-            if request.FILES:
-                return request.POST
-            else:
-                return simplejson.loads(req, "ISO-8859-1")
-        except Exception as e:
-            log.error("Error json-decoding input : " +e.message)
-            return None
-    else:
-        return None
 
 
 
