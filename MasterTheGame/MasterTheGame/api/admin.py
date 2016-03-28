ec6752a2dab63dbac14018732e4ee22b1004443a
sreejit pv <sreejith.pv@sports4you.com>
2016-03-28 22:04:35 +0530

    admin

--- MasterTheGame/MasterTheGame/api/admin.py
+++ MasterTheGame/MasterTheGame/api/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from api.models import User
+from api.models import Player
 
 
 
 
 
-admin.site.register(User)
+admin.site.register(Player)
 # Register your models here.
